5aa26a6c025d1e1edd2bf216349ba40c86fe2bad
Khanh Nguyen Tran Nha <nhakhanhntn@gmail.com>
2025-03-22 15:37:49 +0700

    riup

--- Lab8NET1051/ClientProduct/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace ClientProduct.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
